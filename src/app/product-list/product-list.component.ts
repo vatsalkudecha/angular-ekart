e369f05337ca4aea49f040d526e927603ccccf29
vatsalkudecha <1311vatsal@gmail.com>
2024-08-24 01:46:56 +0530

    ngFor Directives

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,35 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  // name = 'John Doe'
-  addToCart: number = 0
-  product = {
-    name: 'iPhone 15',
-    price: 1000,
-    color:'Red',
-    discount: 10,
-    inStock: 5,
-    pImage: '../../assets/images/iphone.png'
-  }
-
-  getDiscountedPrice() {
-    return this.product.price * (100-this.product.discount)/100;
-  }
-
-  onNameChange(event: any) {
-    // console.log(event)
-    // this.name = event.target.value
-  }
-
-  incrementCartValue() {
-    if(this.addToCart < this.product.inStock) {
-      this.addToCart++;
-    }
-  }
-
-  decrementCartValue() {
-    if(this.addToCart > 0){
-      this.addToCart--;
-    }
-  }
+  // listOfString: string[] = ['Vatsal', 'Dhyey', 'Sahil', 'Kash'];
 }
