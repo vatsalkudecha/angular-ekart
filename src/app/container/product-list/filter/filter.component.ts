ed3aa22b6bcac8cbe1e3da3835a26c6a69cb449f
vatsalkudecha <1311vatsal@gmail.com>
2024-08-27 19:37:05 +0530

    @Output for custom event binding: child -> parent

---
+++ src/app/container/product-list/filter/filter.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css',
+})
+export class FilterComponent {
+  
+  @Input()
+  all: number = 0;
+  
+  @Input()
+  inStock: number = 0;
+  
+  @Input()
+  outOfStock: number = 0;
+
+  @Output()
+  selectedFilterRadioButtonChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  selectedFilterRadioButton: string = 'all';
+
+  onSelectedFilterRadioButtonChanged() {
+    // console.log('selectedFilterRadioButtonChanged event raised');
+    this.selectedFilterRadioButtonChanged.emit(this.selectedFilterRadioButton);
+  }
+}
--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -598,4 +598,15 @@ export class ProductListComponent {
       slug: 'michael-feburary-sk8-hi',
     },
   ];
+
+  totalProductCount = this.products.length;
+  totalProductInStock = this.products.filter(prod => prod.is_in_inventory == true).length;
+  totalProductOutOfStock = this.products.filter(prod => prod.is_in_inventory === false).length;
+
+  selectedFilterRadioButton: string = 'all'
+
+  onFilterChanged(value: string) {
+    // console.log(value);
+    this.selectedFilterRadioButton = value;
+  }
 }
