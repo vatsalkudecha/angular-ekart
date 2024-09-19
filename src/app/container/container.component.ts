b01e6539754db9c19680dcc8309ae84681a31b29
vatsalkudecha <1311vatsal@gmail.com>
2024-09-19 16:55:00 +0530

    ViewChild in component

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-container',
@@ -9,6 +10,8 @@ export class ContainerComponent {
   listOfString: string[] = ['Vatsal', 'Dhyey', 'Sahil', 'Kash'];
 
   searchText: string = '';
+
+  @ViewChild(ProductListComponent) productListComponent: ProductListComponent;
 
   setSearchText(value: string){
     this.searchText = value;
--- src/app/container/product-detail/product-detail.component.ts
+++ src/app/container/product-detail/product-detail.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../Models/Product';
+import { ProductListComponent } from '../product-list/product-list.component';
 
 @Component({
   selector: 'product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  @Input() productListComp: ProductListComponent = undefined;
+
   product: Product;
+
+  ngOnInit(): void {
+    this.product = this.productListComp.selectedProduct;
+  }
 }
